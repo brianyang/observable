5ef43ff26d8eba7cc5f5bc6a9d5ac797cb5e9f6a
Brian Yang <brianyang1@gmail.com>
2012-11-11 06:32:40 -0500

    implement singleton that returns an object

--- scripts/common.js
+++ scripts/common.js
@@ -1,6 +1,25 @@
 OBSERVABLE = (function(){
 
+  var o = []
+  console.log('init observable')
+  pubsub.subscribe()
 
+  o.subscribe = {
+    init: function(){
+      console.log('subscribe.init')
+    }
+  }
+, o.publish = {
+    init: function(){
+      console.log('publish.init')
+    }
+  }
+, o.unsubscribe = {
+    init: function(){
+      console.log('unsubscribe.init')
+    }
+  }
+  return o
 
 }())
 
